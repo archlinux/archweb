28bc93261ef027d5c175de3d87c15b818543e151
eliott <eliott@cactuswax.net>
2007-12-29 14:53:21 -0800

    removed helper script

--- scripts/djangoshell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/sites/archlinux/archlinux
-export PYTHONPATH=/usr/local/django:/home/sites/archlinux:${PYTHONPATH}
-export DJANGO_SETTINGS_MODULE=archlinux.settings
-python manage.py $*
-
