824004e7db73931c83f8ed45b48fbee782dfae59
eliott <eliott@cactuswax.net>
2007-12-29 14:51:13 -0800

    Moved admin media under media dir. It helps with both serving static files during a testing scenario, to easing deployment.

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ SITE_ID = 1
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin-media/'
+ADMIN_MEDIA_PREFIX = '/media/admin_media/'
 
 # URL to send users when they don't have sufficient privileges
 BADPRIVS_URL = '/denied/'
