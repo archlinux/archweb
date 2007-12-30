89659cef5f309d7eb6bc6ca3d4ab84b779c6728d
eliott <eliott@cactuswax.net>
2007-12-29 16:53:03 -0800

    Removed import no longer used.

--- news/models.py
+++ news/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import re
-from archweb_dev.utils import Stripper
 
 class News(models.Model):
     id = models.AutoField(primary_key=True)
