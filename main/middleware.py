ca033e91bf5e6c22369447a0f1c7fdd4e497b736
eliott <eliott@cactuswax.net>
2008-04-09 22:42:56 -0700

    Added/changed full site login middleware.

---
+++ main/middleware.py
@@ -0,0 +1,52 @@
+###
+# Copyright (c) 2006-2007, Jared Kuolt
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice, 
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright 
+#       notice, this list of conditions and the following disclaimer in the 
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the SuperJared.com nor the names of its 
+#       contributors may be used to endorse or promote products derived from 
+#       this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+###
+
+from django.conf import settings
+from django.contrib.auth.views import login
+from django.http import HttpResponseRedirect
+import re
+
+class RequireLoginMiddleware(object):
+    """
+    Require Login middleware. If enabled, each Django-powered page will
+    require authentication.
+    
+    If an anonymous user requests a page, he/she is redirected to the login
+    page set by LOGIN_URL.
+    """
+    def __init__(self):
+        self.exceptionre = re.compile("(^/media/)|(^/robots.txt)|(^/login/)")
+    
+    def process_request(self, request):
+        if request.user.is_anonymous() and not self.exceptionre.search(request.path):
+            if request.POST:
+                return login(request)
+            else:
+                return HttpResponseRedirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
