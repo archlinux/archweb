bd2344b0b5ac150602508a91000ff6a1448ede9a
Dusty Phillips <buchuki@gmail.com>
2008-07-01 18:56:54 -0400

    missing import

--- todolists/views.py
+++ todolists/views.py
@@ -1,6 +1,7 @@
 import django.newforms as forms
 
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import permission_required
