22f22ef66927a09b8eda4d731d5af15e0c42a345
Dusty Phillips <buchuki@gmail.com>
2008-10-07 11:59:31 -0400

    move validation template tage temporarily; it will be removed eventually

--- packages/templatetags/package_extras.py
+++ packages/templatetags/package_extras.py
@@ -30,5 +30,14 @@ def do_buildsortqs(parser, token):
 def space2br(value):
     return value.replace(' ', '<br />')
 
+@register.inclusion_tag('errors.html')
+def print_errors(errors):
+    errs = []
+    for e,msg in errors.iteritems():
+        errmsg = str(msg[0])
+        # hack -- I'm a python idiot
+        errs.append( (e, errmsg[2:-2]) )
+    return {'errors': errs}
+
 # vim: set ts=4 sw=4 et:
 
