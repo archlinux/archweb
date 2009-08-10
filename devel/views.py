9cc1ac8e46697f57a069a1db5a1688521581cf0b
Dusty Phillips <buchuki@gmail.com>
2009-08-10 16:02:41 -0400

    Sometimes its better if you test your code before deploying. Other times, you just fix the missing imports in a second commit.

--- devel/views.py
+++ devel/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.mail import send_mail
 from archweb_dev.main.models import Package, Todolist
 from archweb_dev.main.models import Arch, Repo
 from archweb_dev.main.models import UserProfile, News
