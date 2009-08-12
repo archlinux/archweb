88d83dfaa66d6a01621dcff61b441babdf4833ce
Dusty Phillips <buchuki@gmail.com>
2009-08-12 15:39:56 -0600

    Add a couple columns to user admin at Aaron's request.

--- main/admin.py
+++ main/admin.py
@@ -79,6 +79,8 @@ class UserProfileInline(admin.StackedInline):
 
 class UserProfileAdmin(UserAdmin):
     inlines = [UserProfileInline]
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
 
 
 admin.site.register(User, UserProfileAdmin)
