e0506afaf1b876d672e5cb40ad13702f0c6f0e01
Ismael Carnales <icarnales@gmail.com>
2009-12-01 23:02:25 -0200

    Add basic tests to public app
    The addition of a models.py file in public app is needed for django to
    recognize it as an application for testing.
    
    Signed-off-by: Ismael Carnales <icarnales@gmail.com>

---
+++ public/tests.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+
+
+class PublicTest(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_art(self):
+        response = self.client.get('/art/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_svn(self):
+        response = self.client.get('/svn/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_developers(self):
+        response = self.client.get('/developers/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_fellows(self):
+        response = self.client.get('/fellows/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_donate(self):
+        response = self.client.get('/donate/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_download(self):
+        response = self.client.get('/download/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_irc(self):
+        response = self.client.get('/irc/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_moreforums(self):
+        response = self.client.get('/moreforums/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_projects(self):
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, 200)
