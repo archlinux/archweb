c52d9649218107ed36cdc816b5d872cb030f367c
Dan McGee <dan@archlinux.org>
2010-02-09 10:40:07 -0600

    Fix the get_user middleware stuff
    First off, the copyright notice is from the other middleware previously
    removed, so all of that can go, along with some stragglers in the import
    list.
    
    Next, as stated on
    http://code.djangoproject.com/wiki/CookBookThreadlocalsAndUser, you need to
    be sure to have the import in the model class be the same as the import used
    in the middleware declaration, which was not true. Whoops.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
-from archweb.main.middleware import get_user
+from main.middleware import get_user
 
 ###########################
 ### User Profile Class ####
