3394a94a4019406b643341771dae057d39af73e3
Dan McGee <dan@archlinux.org>
2010-09-14 18:41:29 -0500

    Re-enable etags
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- settings.py
+++ settings.py
@@ -33,8 +33,6 @@ USE_I18N = False
 # calendars according to the current locale
 USE_L10N = False
 
-USE_ETAGS = False
-
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
