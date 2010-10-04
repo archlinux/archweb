5e509529f00bd50ddfc6638d92c601209664d5d8
Dan McGee <dan@archlinux.org>
2010-10-04 13:29:01 -0500

    reporead: ignore nicknames in name matching code

--- devel/management/commands/reporead.py
+++ devel/management/commands/reporead.py
@@ -137,6 +137,10 @@ def find_user(userstring):
         # one of the two name fields.
         name_q = Q()
         for token in name.split():
+            # ignore quoted parts; e.g. nicknames in strings
+            if re.match(r'^[\'"].*[\'"]$', token):
+                print "token match:", token
+                continue
             name_q &= (Q(first_name__icontains=token) |
                     Q(last_name__icontains=token))
         return User.objects.get(name_q)
