52662150470140cdea9bf1166265fb36ff4cd7c1
Dan McGee <dan@archlinux.org>
2010-10-27 11:20:22 -0500

    Add tag for inserting CDN prefix on media URLs
    This tag will just be placed before any media reference that is available on
    the CDN; if the CDN is enabled and CDN_PATH/CDN_PATH_SECURE are defined the
    correct URL will be generated.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- main/templatetags/cdn.py
+++ main/templatetags/cdn.py
@@ -9,8 +9,7 @@ def jquery(parser, token):
 
 class JQueryNode(template.Node):
     def render(self, context):
-        # if we have the request in context, we can check if it is secure and
-        # serve content from HTTPS instead.
+        # serve content from HTTPS if we know this request is secure
         secure = 'secure' in context and context['secure']
         prefixes = { False: 'http', True: 'https' }
         version = '1.4.3'
@@ -21,5 +20,22 @@ class JQueryNode(template.Node):
         else:
             jquery = '/media/jquery-%s.min.js' % version
         return '<script type="text/javascript" src="%s"></script>' % jquery
+
+@register.tag
+def cdnprefix(parser, token):
+    return CDNPrefixNode()
+
+class CDNPrefixNode(template.Node):
+    def render(self, context):
+        oncdn = getattr(settings, 'CDN_ENABLED', True)
+        if not oncdn:
+            return ''
+        secure = 'secure' in context and context['secure']
+        # if left undefined, same behavior as if CDN is turned off
+        paths = {
+                False: getattr(settings, 'CDN_PATH', ''),
+                True: getattr(settings, 'CDN_PATH_SECURE', ''),
+        }
+        return paths[secure]
 
 # vim: set ts=4 sw=4 et:
