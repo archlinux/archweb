eeff0749d86ec403e55dc626034991638edad619
Dan McGee <dan@archlinux.org>
2010-12-21 21:23:55 -0600

    Add migration to split package files into parts
    This datamigration is a bit more tricky and we do some DB-specific stuff
    so it can be done a lot faster- we are dealing with millions of rows in
    this migration in production.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

---
+++ main/migrations/0041_move_package_files_data.py
@@ -0,0 +1,186 @@
+# encoding: utf-8
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+from django.db.models import F
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        # Handle the paths that are directories first
+        orm.PackageFile.objects.filter(path__endswith='/').update(
+                is_directory=True, directory=F('path'))
+
+        if db.backend_name == 'mysql':
+            db.execute("UPDATE package_files "
+                    "SET filename = SUBSTRING_INDEX(path, '/', -1), "
+                    "directory = SUBSTRING(path, 1, LENGTH(path) - INSTR(REVERSE(path), '/') + 1) "
+                    "WHERE is_directory = 0")
+        elif db.backend_name == 'postgresql':
+            db.execute("UPDATE package_files "
+                    "SET filename = substring(filename, '.*/(.*?)$'), "
+                    "directory = substring(filename, '.*/') "
+                    "WHERE is_directory = 0")
+        else:
+            # Sucks that we can't always do this in one query at the DB level...
+            files = orm.PackageFile.objects.filter(is_directory=False)
+            for f in files:
+                parts = f.path.rsplit('/', 1)
+                db.execute("UPDATE package_files "
+                        "SET filename = %s, "
+                        "directory = %s "
+                        "WHERE id = %s",
+                        (parts[1], parts[0] + '/', f.id))
+                # We could do this, but then Django takes forever to generate
+                # basically the same query for every row.
+                #f.filename = parts[1]
+                #f.directory = parts[0] + '/'
+                #f.save()
+
+    def backwards(self, orm):
+        orm.PackageFile.objects.all().update(is_directory=False,
+                directory='', filename=None)
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'main.arch': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Arch', 'db_table': "'arches'"},
+            'agnostic': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'main.donor': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Donor', 'db_table': "'donors'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'visible': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'main.package': {
+            'Meta': {'ordering': "('pkgname',)", 'object_name': 'Package', 'db_table': "'packages'"},
+            'arch': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'packages'", 'to': "orm['main.Arch']"}),
+            'build_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'compressed_size': ('django.db.models.fields.BigIntegerField', [], {'null': 'True'}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'files_last_update': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'flag_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'installed_size': ('django.db.models.fields.BigIntegerField', [], {'null': 'True'}),
+            'last_update': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'packager': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'}),
+            'packager_str': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'pkgbase': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'pkgdesc': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'pkgname': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'pkgrel': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'pkgver': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'repo': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'packages'", 'to': "orm['main.Repo']"}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'})
+        },
+        'main.packagedepend': {
+            'Meta': {'object_name': 'PackageDepend', 'db_table': "'package_depends'"},
+            'depname': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'depvcmp': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'optional': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'pkg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['main.Package']"})
+        },
+        'main.packagefile': {
+            'Meta': {'object_name': 'PackageFile', 'db_table': "'package_files'"},
+            'directory': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_directory': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'pkg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['main.Package']"})
+        },
+        'main.repo': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Repo', 'db_table': "'repos'"},
+            'bugs_project': ('django.db.models.fields.SmallIntegerField', [], {'default': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'svn_root': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'testing': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'main.signoff': {
+            'Meta': {'object_name': 'Signoff'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'packager': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'pkg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['main.Package']"}),
+            'pkgrel': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'pkgver': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'main.todolist': {
+            'Meta': {'object_name': 'Todolist', 'db_table': "'todolists'"},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'date_added': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'main.todolistpkg': {
+            'Meta': {'unique_together': "(('list', 'pkg'),)", 'object_name': 'TodolistPkg', 'db_table': "'todolist_pkgs'"},
+            'complete': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['main.Todolist']"}),
+            'pkg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['main.Package']"})
+        },
+        'main.userprofile': {
+            'Meta': {'object_name': 'UserProfile', 'db_table': "'user_profiles'"},
+            'alias': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'allowed_repos': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['main.Repo']", 'symmetrical': 'False', 'blank': 'True'}),
+            'favorite_distros': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interests': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'languages': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'notify': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'occupation': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'other_contact': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'picture': ('django.db.models.fields.files.FileField', [], {'default': "'devs/silhouette.png'", 'max_length': '100'}),
+            'public_email': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'roles': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'time_zone': ('django.db.models.fields.CharField', [], {'default': "'UTC'", 'max_length': '100'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'userprofile'", 'unique': 'True', 'to': "orm['auth.User']"}),
+            'website': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'yob': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['main']
