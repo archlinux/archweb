176d1619023e542cf25a9a52cc61df638df8dd67
Dan McGee <dan@archlinux.org>
2011-01-10 14:43:32 -0600

    Fix old news URLs not redirecting properly
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- news/urls.py
+++ news/urls.py
@@ -4,11 +4,11 @@ urlpatterns = patterns('news.views',
     (r'^$',                         'news_list', {}, 'news-list'),
     (r'^add/$',                     'add'),
     (r'^preview/$',                 'preview'),
+    # old news URLs, permanent redirect view so we don't break all links
+    (r'^(?P<object_id>\d+)/$',      'view_redirect'),
     (r'^(?P<slug>[-\w]+)/$',        'view'),
     (r'^(?P<slug>[-\w]+)/edit/$',   'edit'),
     (r'^(?P<slug>[-\w]+)/delete/$', 'delete'),
-    # old news URLs, permanent redirect view so we don't break all links
-    (r'^(?P<object_id>\d+)/$',      'view_redirect'),
 )
 
 # vim: set ts=4 sw=4 et:
