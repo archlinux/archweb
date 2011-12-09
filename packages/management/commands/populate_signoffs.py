4fa709ea86c8eefac05a848187fc7281edff8fa9
Dan McGee <dan@archlinux.org>
2011-12-09 09:28:49 -0600

    populate_signoffs: add an SVN log cache for duplicate lookups
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- packages/management/commands/populate_signoffs.py
+++ packages/management/commands/populate_signoffs.py
@@ -44,6 +44,9 @@ class Command(NoArgsCommand):
         return add_signoff_comments()
 
 def svn_log(pkgbase, repo):
+    '''Retrieve the most recent SVN log entry for the given pkgbase and
+    repository. The configured setting SVN_BASE_URL is used along with the
+    svn_root for each repository to form the correct URL.'''
     path = '%s%s/%s/trunk/' % (settings.SVN_BASE_URL, repo.svn_root, pkgbase)
     cmd = ['svn', 'log', '--limit=1', '--xml', path]
     log_data = subprocess.check_output(cmd)
@@ -58,6 +61,17 @@ def svn_log(pkgbase, repo):
         'author': xml.findtext('logentry/author'),
         'message': xml.findtext('logentry/msg'),
     }
+
+def cached_svn_log(pkgbase, repo):
+    '''Retrieve the cached version of the SVN log if possible, else delegate to
+    svn_log() to do the work and cache the result.'''
+    key = (pkgbase, repo)
+    if key in cached_svn_log.cache:
+        return cached_svn_log.cache[key]
+    log = svn_log(pkgbase, repo)
+    cached_svn_log.cache[key] = log
+    return log
+cached_svn_log.cache = {}
 
 def create_specification(package, log, finder):
     trimmed_message = log['message'].strip()
@@ -80,7 +94,7 @@ def add_signoff_comments():
             continue
 
         logger.debug("getting SVN log for %s (%s)", group.pkgbase, group.repo)
-        log = svn_log(group.pkgbase, group.repo)
+        log = cached_svn_log(group.pkgbase, group.repo)
         logger.info("creating spec with SVN message for %s", group.pkgbase)
         spec = create_specification(group.packages[0], log, finder)
         spec.save()
