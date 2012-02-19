f451e61725214337253f826ec62800ef7faa4995
Dan McGee <dan@archlinux.org>
2012-02-18 20:40:15 -0600

    Add retro views from 2003 through 2011
    These follow the prior commit adding the framework and an initial 2002
    retro view.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- retro/views.py
+++ retro/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import Http404
 from django.views.decorators.cache import cache_page
 from django.views.generic.simple import direct_to_template
@@ -5,6 +6,15 @@ from django.views.generic.simple import direct_to_template
 
 RETRO_YEAR_MAP = {
     2002: 'index-20020328.html',
+    2003: 'index-20030330.html',
+    2004: 'index-20040327.html',
+    2005: 'index-20050328.html',
+    2006: 'index-20060328.html',
+    2007: 'index-20070324.html',
+    2008: 'index-20080311.html',
+    2009: 'index-20090327.html',
+    2010: 'index-20100208.html',
+    2011: 'index-20110212.html',
 }
 
 
@@ -14,6 +24,10 @@ def retro_homepage(request, year):
     template = RETRO_YEAR_MAP.get(year, None)
     if template is None:
         raise Http404
-    return direct_to_template(request, 'retro/%s' % template, {})
+    static_url = '%s%d/' % (settings.STATIC_URL, year)
+    context = {
+        'RETRO_STATIC_URL': static_url,
+    }
+    return direct_to_template(request, 'retro/%s' % template, context)
 
 # vim: set ts=4 sw=4 et:
