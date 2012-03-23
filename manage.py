7896779ff18b304f8246c24123b8cbf9b82ec5b0
Dan McGee <dan@archlinux.org>
2012-03-23 17:46:10 -0500

    Bump requirements to Django 1.4 and add new manage.py
    The default manage.py script has been updated in Django 1.4, and the old
    version and functions it calls is deprecated.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- manage.py
+++ manage.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python2
-
-from django.core.management import execute_manager
-
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
+import os
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
 
 # vim: set ts=4 sw=4 et:
-
