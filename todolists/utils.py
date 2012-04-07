9684e7c095585059033ac29339b058931a6547e3
Dan McGee <dan@archlinux.org>
2012-04-07 14:51:25 -0500

    Prevent selection of many useless fields when getting todolists
    This is a bit of a hack, but makes the resulting resultset returned from
    the database a lot smaller and kills off all the columns we don't care
    about and would never look at.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- todolists/utils.py
+++ todolists/utils.py
@@ -2,9 +2,13 @@ from django.db.models import Count
 
 from main.models import Todolist
 
+
 def get_annotated_todolists():
     qs = Todolist.objects.all()
-    lists = qs.select_related('creator').annotate(
+    lists = qs.select_related('creator').defer(
+            'creator__email', 'creator__password', 'creator__is_staff',
+            'creator__is_active', 'creator__is_superuser',
+            'creator__last_login', 'creator__date_joined').annotate(
             pkg_count=Count('todolistpkg')).order_by('-date_added')
     incomplete = qs.filter(todolistpkg__complete=False).annotate(
             Count('todolistpkg')).values_list('id', 'todolistpkg__count')
