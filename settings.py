4c446e5cf71e6755f87ca08cf851569a2c13614b
Dan McGee <dan@archlinux.org>
2012-07-28 11:45:08 -0500

    Convert releng URLs to https by default
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- settings.py
+++ settings.py
@@ -127,10 +127,10 @@ INSTALLED_APPS = (
 PGP_SERVER = 'pgp.mit.edu:11371'
 
 # URL to fetch a current list of available ISOs
-ISO_LIST_URL = 'http://releng.archlinux.org/isos/'
+ISO_LIST_URL = 'https://releng.archlinux.org/isos/'
 
 # URL to the PXE netboot instructions
-PXEBOOT_URL = 'http://releng.archlinux.org/pxeboot/'
+PXEBOOT_URL = 'https://releng.archlinux.org/pxeboot/'
 
 # URL for SVN access for fetching commit messages (note absence of packages or
 # community bit on the end, repo.svn_root is appended)
