3422efd3073df0b32833aee26a6aee7018010f4e
Dan McGee <dan@archlinux.org>
2012-10-18 08:58:17 -0500

    Modernize initial main migration
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -1,9 +1,9 @@
-
+# encoding: utf-8
 from south.db import db
+from south.v2 import SchemaMigration
 from django.db import models
-from main.models import *
 
-class Migration:
+class Migration(SchemaMigration):
     
     def forwards(self, orm):
         
