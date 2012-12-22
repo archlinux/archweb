8be27a5cafde87c439b19f64a7c215410c88484b
Dan McGee <dan@archlinux.org>
2012-12-21 19:26:35 -0600

    Add new todolists and todolist package model
    Move the todolist model from main to the todolists application, and make
    a few minor tweaks to field names along the way. Also add a 'raw' field
    that will hold the originally input text data from the creator or last
    modifier of the todolist.
    
    Add pkgname, pkgbase, arch, and repo fields to a new todolist package
    model, which will supplement the former foreign key to an actual package
    object. This will prevent todolist package objects from ever being
    deleted as they can be now, which is not intuitive.
    
    Also change the current boolean 'complete' flag to a 'status' enum that
    can hold other values. For now, we add 'In-progress' to the mix.
    
    Finally, add a 'user' field, and a 'comments' field that will be
    utilized later by the UI.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

---
+++ todolists/models.py
@@ -0,0 +1,81 @@
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.db import models
+from django.db.models import Q
+from django.db.models.signals import pre_save
+
+from main.models import Arch, Repo, Package
+from main.utils import set_created_field
+
+
+class TodolistManager(models.Manager):
+    def incomplete(self):
+        not_done = (Q(todolistpackage__status=TodolistPackage.INCOMPLETE) |
+                Q(todolistpackage__status=TodolistPackage.IN_PROGRESS))
+        return self.order_by().filter(not_done).distinct()
+
+
+class Todolist(models.Model):
+    old_id = models.IntegerField(null=True, unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.PROTECT,
+            related_name="created_todolists")
+    created = models.DateTimeField(db_index=True)
+    last_modified = models.DateTimeField(editable=False)
+    raw = models.TextField(blank=True)
+
+    objects = TodolistManager()
+
+    class Meta:
+        get_latest_by = 'created'
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return '/todo/%i/' % self.id
+
+    def get_full_url(self, proto='https'):
+        '''get a URL suitable for things like email including the domain'''
+        domain = Site.objects.get_current().domain
+        return '%s://%s%s' % (proto, domain, self.get_absolute_url())
+
+
+class TodolistPackage(models.Model):
+    INCOMPLETE = 0
+    COMPLETE = 1
+    IN_PROGRESS = 2
+    STATUS_CHOICES = (
+        (INCOMPLETE, 'Incomplete'),
+        (COMPLETE, 'Complete'),
+        (IN_PROGRESS, 'In-progress'),
+    )
+
+    todolist = models.ForeignKey(Todolist)
+    pkg = models.ForeignKey(Package, null=True, on_delete=models.SET_NULL)
+    pkgname = models.CharField(max_length=255)
+    pkgbase = models.CharField(max_length=255)
+    arch = models.ForeignKey(Arch)
+    repo = models.ForeignKey(Repo)
+    created = models.DateTimeField()
+    status = models.SmallIntegerField(default=0, choices=STATUS_CHOICES)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    comments = models.TextField(null=True, blank=True)
+
+    class Meta:
+        unique_together = (('todolist','pkgname', 'arch'),)
+
+    def __unicode__(self):
+        return self.pkgname
+
+    def status_css_class(self):
+        return self.get_status_display().lower().replace('-', '')
+
+
+pre_save.connect(set_created_field, sender=Todolist,
+        dispatch_uid="todolists.models")
+pre_save.connect(set_created_field, sender=TodolistPackage,
+        dispatch_uid="todolists.models")
+
+# vim: set ts=4 sw=4 et:
