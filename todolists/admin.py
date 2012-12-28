f167d6e6e8ba2ca5e29f6a1a82b0e273ffec13ab
Dan McGee <dan@archlinux.org>
2012-12-27 21:03:09 -0600

    Add todolist admin for new model
    Signed-off-by: Dan McGee <dan@archlinux.org>

---
+++ todolists/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Todolist
+
+
+class TodolistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator', 'created', 'description')
+    search_fields = ('name', 'description')
+    date_hierarchy = 'created'
+
+
+admin.site.register(Todolist, TodolistAdmin)
+
+# vim: set ts=4 sw=4 et:
