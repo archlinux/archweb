7bb5c54d7ee3112eaa827bad5b2be10a017dbb3e
Dan McGee <dan@archlinux.org>
2012-12-31 09:42:16 -0600

    Minor news admin code cleanup
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- news/admin.py
+++ news/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 
 from .models import News
 
+
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'postdate', 'last_modified', 'safe_mode')
     list_filter = ('postdate', 'author', 'safe_mode')
     search_fields = ('title', 'content')
     date_hierarchy = 'postdate'
 
+
 admin.site.register(News, NewsAdmin)
+
+# vim: set ts=4 sw=4 et:
