58a4ecb0baca75e0acd67396c3564a1e0b1bb4b5
Dan McGee <dan@archlinux.org>
2012-12-31 09:42:06 -0600

    Small admin tweaks for todolists filtering
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- todolists/admin.py
+++ todolists/admin.py
@@ -5,6 +5,7 @@ from .models import Todolist
 
 class TodolistAdmin(admin.ModelAdmin):
     list_display = ('name', 'creator', 'created', 'description')
+    list_filter = ('created', 'creator')
     search_fields = ('name', 'description')
     date_hierarchy = 'created'
 
