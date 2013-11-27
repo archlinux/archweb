954b61ee3a0760990f8e17d7d692f3cabd949f9f
Dan McGee <dan@archlinux.org>
2013-11-27 17:08:42 -0500

    Syntax cleanups to main admin
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- main/admin.py
+++ main/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
 from main.models import Arch, Donor, Package, Repo
 
+
 class DonorAdmin(admin.ModelAdmin):
     list_display = ('name', 'visible', 'created')
     list_filter = ('visible', 'created')
     search_fields = ('name',)
     exclude = ('created',)
 
+
 class ArchAdmin(admin.ModelAdmin):
     list_display = ('name', 'agnostic', 'required_signoffs')
     list_filter = ('agnostic',)
     search_fields = ('name',)
+
 
 class RepoAdmin(admin.ModelAdmin):
     list_display = ('name', 'testing', 'staging', 'bugs_project',
             'bugs_category', 'svn_root')
     list_filter = ('testing', 'staging')
     search_fields = ('name',)
+
 
 class PackageAdmin(admin.ModelAdmin):
     list_display = ('pkgname', 'full_version', 'repo', 'arch', 'packager',
