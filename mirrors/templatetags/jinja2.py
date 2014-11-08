713ab837ba59c4a7c0b60cb8e8be4b27f4520e52
Dan McGee <dan@archlinux.org>
2014-11-08 14:00:06 -0600

    Convert mirror status tables to Jinja2
    Yay for way improved performance. Local testing showed render times
    going from 265 ms to 135 ms.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- mirrors/templatetags/jinja2.py
+++ mirrors/templatetags/jinja2.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django_jinja import library
 from markupsafe import Markup
 
@@ -23,9 +24,30 @@ def duration(value):
 
 
 @library.filter
+def hours(value):
+    if not value and type(value) != timedelta:
+        return u''
+    # does not take microseconds into account
+    total_secs = value.seconds + value.days * 24 * 3600
+    mins = total_secs // 60
+    hrs, mins = divmod(mins, 60)
+    if hrs == 1:
+        return '%d hour' % hrs
+    return '%d hours' % hrs
+
+
+@library.filter
 def floatvalue(value, arg=2):
     if value is None:
         return u''
     return '%.*f' % (arg, value)
+
+
+@library.filter
+def percentage(value, arg=1):
+    if not value and type(value) != float:
+        return u''
+    new_val = value * 100.0
+    return '%.*f%%' % (arg, new_val)
 
 # vim: set ts=4 sw=4 et:
