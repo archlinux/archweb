1fb790594c425f220859f9618a80452693297b80
Johannes Löthberg <johannes@kyriasis.com>
2017-12-30 03:30:24 +0100

    Update package fixture to be the same as devel/fixtures/core.db.tar.gz
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- packages/tests.py
+++ packages/tests.py
@@ -75,8 +75,9 @@ class PackageSearchJson(TestCase):
         response = self.client.get('/packages/search/json/?repository=core')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEqual(len(data['results']), 1)
-        self.assertEqual(data['results'][0]['pkgname'], 'linux')
+        self.assertEqual(len(data['results']), 5)
+        self.assertEqual(set(map(lambda r: r['pkgname'], data['results'])),
+                         {"coreutils", "glibc", "linux", "pacman", "systemd"})
 
     def test_packagename(self):
         response = self.client.get('/packages/search/json/?name=linux')
@@ -113,7 +114,7 @@ class PackageSearch(TestCase):
     def test_filter_repo(self):
         response = self.client.get('/packages/?repo=Core')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('1 matching package found', response.content)
+        self.assertIn('5 matching packages found', response.content)
 
     def test_filter_desc(self):
         response = self.client.get('/packages/?desc=kernel')
@@ -128,7 +129,7 @@ class PackageSearch(TestCase):
     def test_filter_not_flagged(self):
         response = self.client.get('/packages/?flagged=Not Flagged')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('1 matching package found', response.content)
+        self.assertIn('5 matching packages found', response.content)
 
     def test_filter_arch(self):
         response = self.client.get('/packages/?arch=any')
@@ -138,11 +139,11 @@ class PackageSearch(TestCase):
     def test_filter_maintainer_orphan(self):
         response = self.client.get('/packages/?maintainer=orphan')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('1 matching package found', response.content)
+        self.assertIn('5 matching packages found', response.content)
 
     def test_filter_packager_unknown(self):
         response = self.client.get('/packages/?packager=unknown')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('1 matching package found', response.content)
+        self.assertIn('5 matching packages found', response.content)
 
 # vim: set ts=4 sw=4 et:
