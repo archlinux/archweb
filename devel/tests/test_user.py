f59caea0df0b6d5c2320097477176498889f354a
Johannes Löthberg <johannes@kyriasis.com>
2018-01-02 09:06:36 +0100

    devel/tests/test_user: Fix tests to now expect a relative URL
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- devel/tests/test_user.py
+++ devel/tests/test_user.py
@@ -10,21 +10,21 @@ class DevelTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.has_header('Location'), True)
         self.assertEqual(response['location'],
-                         'http://testserver/login/?next=/devel/')
+                         '/login/?next=/devel/')
 
     def test_profile(self):
         response = self.client.get('/devel/profile/')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.has_header('Location'), True)
         self.assertEqual(response['location'],
-                         'http://testserver/login/?next=/devel/profile/')
+                         '/login/?next=/devel/profile/')
 
     def test_newuser(self):
         response = self.client.get('/devel/newuser/')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.has_header('Location'), True)
         self.assertEqual(response['location'],
-                         'http://testserver/login/?next=/devel/newuser/')
+                         '/login/?next=/devel/newuser/')
 
     def test_mirrors(self):
         response = self.client.get('/mirrors/')
