5e874a1832f34805fb74d3d7ad465b13dc3d65af
Jelle van der Waa <jelle@vdwaa.nl>
2018-01-04 21:49:33 +0100

    mirrors: Add mirrorlist urls

--- mirrors/tests.py
+++ mirrors/tests.py
@@ -26,6 +26,11 @@ class MirrorUrlTest(TestCase):
         expected = '/mirrors/%s/%d/' % (self.mirror_url.mirror.name, self.mirror_url.pk)
         self.assertEqual(absolute_url, expected)
 
+    def test_mirror_overview(self):
+        response = self.client.get('/mirrors/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.mirror_url.mirror.name, response.content)
+
     def testClean(self):
         # TODO: add test for self.mirror_url.clean()
         pass
@@ -60,3 +65,41 @@ class MirrorStatusTest(TestCase):
             self.assertEqual(len(data['urls']), 1)
             mirror = data['urls'][0]
             self.assertEqual(mirror['url'], mirror_url.url)
+
+
+class MirrorListTest(TestCase):
+    def setUp(self):
+        self.mirror_url = create_mirror_url()
+
+    def test_mirrorlist(self):
+        response = self.client.get('/mirrorlist/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mirrorlist(self):
+        response = self.client.get('/mirrorlist/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mirrorlist_all(self):
+        response = self.client.get('/mirrorlist/all/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.mirror_url.hostname, response.content)
+
+    def test_mirrorlist_all_http(self):
+        response = self.client.get('/mirrorlist/all/http/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.mirror_url.hostname, response.content)
+
+    def test_mirrorlist_all_https(self):
+        response = self.client.get('/mirrorlist/all/https/')
+        self.assertEqual(response.status_code, 404)
+        # TODO: test 200 case
+
+    def test_mirrorlist_filter(self):
+        response = self.client.get('/mirrorlist/?country=all&protocol=http&ip_version=4')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.mirror_url.hostname, response.content)
+
+    def test_generate(self):
+        response = self.client.get('/mirrorlist/?country=all&protocol=http&ip_version=4')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.mirror_url.hostname, response.content)
