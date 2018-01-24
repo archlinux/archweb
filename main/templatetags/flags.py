6c1c0522f58bb3925f9ea3ac0bbc919ca103252e
Jelle van der Waa <jelle@vdwaa.nl>
2018-01-24 23:13:01 +0100

    main: remove duplicate duration implementation
    The duration templatetag filter is also defined in mirror_status.py

--- main/templatetags/flags.py
+++ main/templatetags/flags.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 from django import template
 
 register = template.Library()
@@ -10,16 +9,6 @@ def country_flag(country):
         return ''
     return '<span class="fam-flag fam-flag-%s" title="%s"></span> ' % (
             unicode(country.code).lower(), unicode(country.name))
-
-@register.filter
-def duration(value):
-    if not value and type(value) != timedelta:
-        return u''
-    # does not take microseconds into account
-    total_secs = value.seconds + value.days * 24 * 3600
-    mins = total_secs // 60
-    hrs, mins = divmod(mins, 60)
-    return '%d:%02d' % (hrs, mins)
 
 
 # vim: set ts=4 sw=4 et:
