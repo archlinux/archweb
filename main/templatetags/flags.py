ec256a35f06ec196804a1bf7986e90a78d85032b
Jelle van der Waa <jelle@vdwaa.nl>
2018-01-24 22:40:22 +0100

    main: Remove duplicate hours filter
    The hours filter is also defined in the mirror_status and only used in
    mirrors.

--- main/templatetags/flags.py
+++ main/templatetags/flags.py
@@ -29,16 +29,4 @@ def duration(value):
     return '%d:%02d' % (hrs, mins)
 
 
-@register.filter
-def hours(value):
-    if not value and type(value) != timedelta:
-        return u''
-    # does not take microseconds into account
-    total_secs = value.seconds + value.days * 24 * 3600
-    mins = total_secs // 60
-    hrs, mins = divmod(mins, 60)
-    if hrs == 1:
-        return '%d hour' % hrs
-    return '%d hours' % hrs
-
 # vim: set ts=4 sw=4 et:
