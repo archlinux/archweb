e46f7a851f0442079da280ecabe8ad950d83d651
Jelle van der Waa <jelle@vdwaa.nl>
2018-02-17 00:44:23 +0100

    releng: Add tests for a few views
    Add a few simple tests for some of the releases views.

---
+++ releng/tests/test_views.py
@@ -0,0 +1,32 @@
+import json
+
+from django.test import TestCase
+
+from releng.models import Release
+
+
+class RelengViewTest(TestCase):
+    fixtures = ['releng/fixtures/release.json']
+
+    def setUp(self):
+        self.release = Release.objects.first()
+
+    def test_release_json(self):
+        version = self.release.version
+        response = self.client.get('/releng/releases/json/')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        
+        self.assertEqual(data['version'], 1)
+        release = data['releases'][0]
+        self.assertEqual(release['version'], version)
+
+    def test_netboot_page(self):
+        response = self.client.get('/releng/netboot/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_release_torrent_not_found(self):
+        # TODO: Add torrent data to release fixture
+        version = self.release.version
+        response = self.client.get('/releng/releases/{}/torrent/'.format(version))
+        self.assertEqual(response.status_code, 404)
