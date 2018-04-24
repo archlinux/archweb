701c3c401c0523ab91cf867730c2565c27fc112f
Frank Vanderham <twelve.eighty@gmail.com>
2018-04-23 20:48:12 -0600

    Fix Python 2 vs. Python 3 Unicode string issues
    * Fixed issue with string concatenation that fails under Python 3
    * Fixed issue with concatenating strings with potentially mixed
    encoding. All strings are now first converted to UTF-8 and then
    concatenated.

--- main/management/commands/donor_import.py
+++ main/management/commands/donor_import.py
@@ -43,10 +43,10 @@ class Command(BaseCommand):
 
     def decode_subject(self, subject):
         subject = decode_header(subject)
-        default_charset = 'ASCII'
+        default_charset = 'utf-8'
         # Convert the list of tuples containing the decoded string and encoding to
-        # the same encoding.
-        return u''.join([unicode(s[0], s[1] or default_charset) for s in subject])
+        # UTF-8
+        return u''.join([s[0].encode(default_charset, 'replace').decode(default_charset, 'replace') for s in subject])
 
 
     def parse_subject(self, subject):
@@ -67,7 +67,7 @@ class Command(BaseCommand):
                 return u''
 
             # Strip any numbers, they could be a bank account number
-            name = filter(lambda x: not x.isdigit(), name)
+            name = u''.join([l for l in name if not l.isdigit()])
 
             # Normalize all capitalized names. (JOHN DOE)
             name = u' '.join(l.capitalize() for l in name.split(u' '))
