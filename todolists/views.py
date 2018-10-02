2c044ad13078bd99f2a6d90fd74a926ff1e61e5d
Jelle van der Waa <jelle@vdwaa.nl>
2018-10-02 13:44:19 +0200

    todolists: remove old_id from todolist model
    Old todolist items contained an old_id to keep links working, these
    items where most recenlty created 6 years ago. Since old todolists of a
    long time ago aren't particularly interesting they will be removed.
    
    Closes #141

--- todolists/views.py
+++ todolists/views.py
@@ -57,11 +57,6 @@ def flag(request, slug, pkg_id):
     return redirect(todolist)
 
 
-def view_redirect(request, old_id):
-    todolist = get_object_or_404(Todolist, old_id=old_id)
-    return redirect(todolist, permanent=True)
-
-
 def view(request, slug):
     todolist = get_object_or_404(Todolist, slug=slug)
     svn_roots = Repo.objects.values_list(
