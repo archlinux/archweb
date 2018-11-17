b45ad187bd60ed0a3f1994651ae2b86797dd2d2b
Jelle van der Waa <jelle@vdwaa.nl>
2018-11-17 18:54:55 +0100

    Update django reverse import

--- devel/models.py
+++ devel/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytz
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User, Group
