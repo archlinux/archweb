32fd54646f0fe60f5165f49a95d21dfbfdb930e6
Jelle van der Waa <jelle@vdwaa.nl>
2019-01-21 20:50:14 +0100

    mirrors: tests: add exception test cases
    Add test cases for 404 and 404 variant url exceptions.

--- mirrors/tests/test_mirrorcheck.py
+++ mirrors/tests/test_mirrorcheck.py
@@ -1,6 +1,8 @@
 import mock
 import time
 
+from urllib.error import HTTPError, URLError
+
 
 from django.utils.timezone import now
 from datetime import timedelta
@@ -56,6 +58,32 @@ class MirrorCheckTest(TestCase):
         MirrorLog.objects.create(url=self.mirror_url, check_time=date)
         call_command('mirrorcheck')
         self.assertEqual(len(MirrorLog.objects.all()), 1)
+
+    @mock.patch('urllib.request.Request')
+    @mock.patch('urllib.request.urlopen')
+    def test_not_found(self, urlopen, Request):
+        excp = HTTPError('https://archlinux.org/404.txt', 404, 'Not Found', '', None)
+        urlopen.return_value.read.side_effect = excp
+        Request.get_host.return_value = 'archlinux.org'
+        Request.type.return_value = 'https'
+
+        call_command('mirrorcheck')
+        mirrorlog = MirrorLog.objects.first()
+        self.assertEqual(mirrorlog.error, str(excp))
+        self.assertEqual(mirrorlog.is_success, False)
+
+    @mock.patch('urllib.request.Request')
+    @mock.patch('urllib.request.urlopen')
+    def test_not_found_variant(self, urlopen, Request):
+        excp = URLError('550 No such file', '550.txt')
+        urlopen.return_value.read.side_effect = excp
+        Request.get_host.return_value = 'archlinux.org'
+        Request.type.return_value = 'https'
+
+        call_command('mirrorcheck')
+        mirrorlog = MirrorLog.objects.first()
+        self.assertIn(excp.reason, mirrorlog.error)
+        self.assertEqual(mirrorlog.is_success, False)
 
     def test_checklocation(self):
         with self.assertRaises(CheckLocation.DoesNotExist) as e:
