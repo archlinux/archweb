6875265c1097d74bc6cad0decfa27ac11bf75789
Jelle van der Waa <jelle@vdwaa.nl>
2019-02-21 19:58:10 +0100

    mirrors: tests: add exception handling tests
    Include more tests for exception handling cases, refactor this later
    using pytest fixture's so there is no need for massive code duplication.

--- mirrors/tests/test_mirrorcheck.py
+++ mirrors/tests/test_mirrorcheck.py
@@ -1,7 +1,10 @@
 import time
 
+from http.client import BadStatusLine
 from unittest import mock
 from urllib.error import HTTPError, URLError
+from ssl import CertificateError
+from socket import timeout, error
 
 
 from django.utils.timezone import now
@@ -75,6 +78,32 @@ class MirrorCheckTest(TestCase):
     @mock.patch('urllib.request.Request')
     @mock.patch('urllib.request.urlopen')
     def test_not_found_variant(self, urlopen, Request):
+        excp = BadStatusLine('')
+        urlopen.return_value.read.side_effect = excp
+        Request.get_host.return_value = 'archlinux.org'
+        Request.type.return_value = 'https'
+
+        call_command('mirrorcheck')
+        mirrorlog = MirrorLog.objects.first()
+        self.assertIn('Exception in processing', mirrorlog.error)
+        self.assertEqual(mirrorlog.is_success, False)
+
+    @mock.patch('urllib.request.Request')
+    @mock.patch('urllib.request.urlopen')
+    def test_cert_error(self, urlopen, Request):
+        excp = CertificateError('certificate error')
+        urlopen.return_value.read.side_effect = excp
+        Request.get_host.return_value = 'archlinux.org'
+        Request.type.return_value = 'https'
+
+        call_command('mirrorcheck')
+        mirrorlog = MirrorLog.objects.first()
+        self.assertIn('certificate error', mirrorlog.error)
+        self.assertEqual(mirrorlog.is_success, False)
+
+    @mock.patch('urllib.request.Request')
+    @mock.patch('urllib.request.urlopen')
+    def test_general_httpexception(self, urlopen, Request):
         excp = URLError('550 No such file', '550.txt')
         urlopen.return_value.read.side_effect = excp
         Request.get_host.return_value = 'archlinux.org'
@@ -83,6 +112,32 @@ class MirrorCheckTest(TestCase):
         call_command('mirrorcheck')
         mirrorlog = MirrorLog.objects.first()
         self.assertIn(excp.reason, mirrorlog.error)
+        self.assertEqual(mirrorlog.is_success, False)
+
+    @mock.patch('urllib.request.Request')
+    @mock.patch('urllib.request.urlopen')
+    def test_socket_timeout(self, urlopen, Request):
+        excp = timeout('timeout')
+        urlopen.return_value.read.side_effect = excp
+        Request.get_host.return_value = 'archlinux.org'
+        Request.type.return_value = 'https'
+
+        call_command('mirrorcheck')
+        mirrorlog = MirrorLog.objects.first()
+        self.assertEqual('Connection timed out.', mirrorlog.error)
+        self.assertEqual(mirrorlog.is_success, False)
+
+    @mock.patch('urllib.request.Request')
+    @mock.patch('urllib.request.urlopen')
+    def test_socket_error(self, urlopen, Request):
+        excp = error('error')
+        urlopen.return_value.read.side_effect = excp
+        Request.get_host.return_value = 'archlinux.org'
+        Request.type.return_value = 'https'
+
+        call_command('mirrorcheck')
+        mirrorlog = MirrorLog.objects.first()
+        self.assertEqual(str(excp), mirrorlog.error)
         self.assertEqual(mirrorlog.is_success, False)
 
     def test_checklocation(self):
