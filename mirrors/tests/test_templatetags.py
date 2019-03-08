7a13e2ecec0625da1e63cc112faef81e994e4c09
Jelle van der Waa <jelle@vdwaa.nl>
2019-03-08 21:56:42 +0100

    mirrors: switch to pytest

--- mirrors/tests/test_templatetags.py
+++ mirrors/tests/test_templatetags.py
@@ -1,27 +1,27 @@
 from datetime import timedelta
 
-from django.test import SimpleTestCase
-
 from mirrors.templatetags.mirror_status import duration, hours, percentage
 
 
-class MirrorTemplateTagTest(SimpleTestCase):
-    def test_duration(self):
-        self.assertEqual(duration(None), u'')
+def test_duration():
+    assert duration(None) == ''
 
-        self.assertEqual(duration(timedelta(hours=5)), '5:00')
-        self.assertEqual(duration(timedelta(hours=5, seconds=61)), '5:01')
-        # Microseconds are skipped
-        self.assertEqual(duration(timedelta(microseconds=9999), ), '0:00')
+    assert duration(timedelta(hours=5)) == '5:00'
+    assert duration(timedelta(hours=5, seconds=61)) == '5:01'
 
-    def test_hours(self):
-        self.assertEqual(hours(None), u'')
+    # Microseconds are skipped
+    assert duration(timedelta(microseconds=9999)) == '0:00'
 
-        self.assertEqual(hours(timedelta(hours=5)), '5 hours')
-        self.assertEqual(hours(timedelta(hours=1)), '1 hour')
-        self.assertEqual(hours(timedelta(seconds=60*60)), '1 hour')
 
-    def test_percentage(self):
-        self.assertEqual(percentage(None), u'')
-        self.assertEqual(percentage(10), '1000.0%')
-        self.assertEqual(percentage(10, 2), '1000.00%')
+def test_hours():
+    assert hours(None) == ''
+
+    assert hours(timedelta(hours=5)) == '5 hours'
+    assert hours(timedelta(hours=1)) == '1 hour'
+    assert hours(timedelta(seconds=60*60)) == '1 hour'
+
+
+def test_percentage():
+    assert percentage(None) == ''
+    assert percentage(10) == '1000.0%'
+    assert percentage(10, 2) == '1000.00%'
