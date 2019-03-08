e5547f358864618fd7ae1561e06aea9279ac70fd
Jelle van der Waa <jelle@vdwaa.nl>
2019-03-08 22:23:26 +0100

    mirrors: convert mirrorstatus to pytest

--- mirrors/tests/test_mirrorstatus.py
+++ mirrors/tests/test_mirrorstatus.py
@@ -1,49 +1,48 @@
-from django.test import TestCase
+def test_mirror_status(db, client):
+        response = client.get('/mirrors/status/')
+        assert response.status_code == 200
 
-from mirrors.tests import create_mirror_url
+
+def test_json_endpoint(client, settings, mirrorurl):
+    settings.CACHES = {'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}}
+    # Disables the cache_function's cache
+
+    response = client.get('/mirrors/status/json/')
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data['urls']) == 1
+    mirror = data['urls'][0]
+    assert mirror['url'] == mirrorurl.url
 
 
-class MirrorStatusTest(TestCase):
-    def test_status(self):
-        response = self.client.get('/mirrors/status/')
-        self.assertEqual(response.status_code, 200)
+def test_json_endpoint_empty(db, client, settings):
+    # Disables the cache_function's cache
+    settings.CACHES = {'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}}
 
-    def test_json_endpoint(self):
-        # Disables the cache_function's cache
-        with self.settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}}):
-            response = self.client.get('/mirrors/status/json/')
-            self.assertEqual(response.status_code, 200)
-            data = response.json()
-            self.assertEqual(data['urls'], [])
+    response = client.get('/mirrors/status/json/')
+    assert response.status_code == 200
+    data = response.json()
+    assert data['urls'] == []
 
-            mirror_url = create_mirror_url()
 
-            response = self.client.get('/mirrors/status/json/')
-            self.assertEqual(response.status_code, 200)
-            data = response.json()
+def test_json_tier_not_found(db, client, settings):
+    response = client.get('/mirrors/status/tier/99/json/')
+    assert response.status_code == 404
 
-            self.assertEqual(len(data['urls']), 1)
-            mirror = data['urls'][0]
-            self.assertEqual(mirror['url'], mirror_url.url)
 
-        mirror_url.delete()
+def test_json_tier_empty_cache(db, client, settings):
+    response = client.get('/mirrors/status/tier/1/json/')
+    assert response.status_code == 200
+    data = response.json()
+    assert data['urls'] == []
 
-    def test_json_tier(self):
-        response = self.client.get('/mirrors/status/tier/99/json/')
-        self.assertEqual(response.status_code, 404)
 
-        response = self.client.get('/mirrors/status/tier/1/json/')
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        self.assertEqual(data['urls'], [])
+def test_json_tier(client, settings, mirrorurl):
+    # Disables the cache_function's cache
+    settings.CACHES={'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}}
 
-        mirror_url = create_mirror_url()
-
-        # Disables the cache_function's cache
-        with self.settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}}):
-            response = self.client.get('/mirrors/status/json/')
-            self.assertEqual(response.status_code, 200)
-            data = response.json()
-            self.assertNotEqual(data['urls'], [])
-
-        mirror_url.delete()
+    response = client.get('/mirrors/status/json/')
+    assert response.status_code == 200
+    data = response.json()
+    assert data['urls'] != []
