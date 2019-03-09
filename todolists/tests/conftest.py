8245939538a80998a86403c970eca174c6bf4292
Jelle van der Waa <jelle@vdwaa.nl>
2019-03-09 22:44:40 +0100

    todolists: port to pytest

---
+++ todolists/tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from main.models import Package
+from todolists.models import Todolist, TodolistPackage
+
+
+NAME = 'Boost rebuild'
+DESCRIPTION = 'Boost 1.66 rebuild'
+RAW = 'linux'
+
+
+@pytest.fixture
+def todolist(admin_user, arches, repos, package):
+    todolist = Todolist.objects.create(name=NAME,
+                                       description=DESCRIPTION,
+                                       creator=admin_user,
+                                       raw=RAW)
+    yield todolist
+    todolist.delete()
+
+
+@pytest.fixture
+def todolistpackage(admin_user, todolist):
+    pkg = Package.objects.first()
+    todopkg = TodolistPackage.objects.create(pkg=pkg, pkgname=pkg.pkgname,
+                                             pkgbase=pkg.pkgbase, arch=pkg.arch,
+                                             repo=pkg.repo, user=admin_user,
+                                             todolist=todolist)
+    yield todopkg
+    todopkg.delete()
--- todolists/tests/test_models.py
+++ todolists/tests/test_models.py
@@ -1,45 +1,19 @@
-from django.contrib.auth.models import User
-from django.test import TestCase
+def test_stripped_description(todolist):
+    todolist.description = 'Boost rebuild '
+    desc = todolist.stripped_description
+    assert desc.endswith(' ') == False
 
 
-from main.models import Package
-from todolists.models import Todolist, TodolistPackage
+def test_get_absolute_url(todolist):
+    assert '/todo/' in todolist.get_absolute_url()
 
 
-class TestTodolist(TestCase):
-    fixtures = ['main/fixtures/arches.json', 'main/fixtures/repos.json',
-                'main/fixtures/package.json']
+def test_get_full_url(todolist):
+    url = todolist.get_full_url()
+    assert 'https://example.com/todo/' in url
 
-    def setUp(self):
-        self.user = User.objects.create(username="joeuser", first_name="Joe",
-                                        last_name="User", email="user1@example.com")
-        self.todolist = Todolist.objects.create(name='Boost rebuild',
-                                                description='Boost 1.66 rebuid',
-                                                creator=self.user,
-                                                raw='linux')
 
-    def tearDown(self):
-        self.todolist.delete()
-        self.user.delete()
-
-    def test_stripped_description(self):
-        self.todolist.description = 'Boost rebuild '
-        desc = self.todolist.stripped_description
-        self.assertFalse(desc.endswith(' '))
-
-    def test_get_absolute_url(self):
-        self.assertIn('/todo/', self.todolist.get_absolute_url())
-
-    def test_get_full_url(self):
-        url = self.todolist.get_full_url()
-        self.assertIn('https://example.com/todo/', url)
-
-    def test_packages(self):
-        pkg = Package.objects.first()
-        todopkg = TodolistPackage.objects.create(pkg=pkg, pkgname=pkg.pkgname,
-                                       pkgbase=pkg.pkgbase, arch=pkg.arch,
-                                       repo=pkg.repo, user=self.user,
-                                       todolist=self.todolist)
-        pkgs = self.todolist.packages()
-        self.assertEqual(len(pkgs), 1)
-        self.assertEqual(pkgs[0], todopkg)
+def test_packages(admin_user, todolist, todolistpackage):
+    pkgs = todolist.packages()
+    assert len(pkgs) == 1
+    assert pkgs[0] == todolistpackage
