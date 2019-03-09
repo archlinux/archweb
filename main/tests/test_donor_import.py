40afe675d23125ab271a8e0a0e3f515425a1c7d3
Jelle van der Waa <jelle@vdwaa.nl>
2019-03-09 16:28:42 +0100

    main: convert tests to pytest

--- main/tests/test_donor_import.py
+++ main/tests/test_donor_import.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import sys
 from email.header import Header
 from email.message import Message
-from io import StringIO
-from tempfile import mkdtemp
+from mailbox import Maildir
 
-from django.test import TransactionTestCase
+import pytest
+
 from django.core.management import call_command
 from django.core.management.base import CommandError
 
@@ -14,67 +13,70 @@ from main.models import Donor
 from main.management.commands.donor_import import Command
 
 
-class DonorImportTest(TransactionTestCase):
+command = Command()
 
-    def setUp(self):
-        self.command = Command()
 
-    def test_parse_subject(self):
-        self.assertIsNone(self.command.parse_subject('garbage'))
+def gen_parse_subject(data):
+    return command.parse_subject(data)
 
-        # Valid
-        valid = u'Receipt [$25.00] By: John Doe [john.doe@archlinux.org]'
-        output = self.command.parse_subject(valid)
-        self.assertEqual(output, u'John Doe')
 
-    def test_parse_name(self):
-        self.assertEqual(self.command.sanitize_name(u'1244'), u'')
-        self.assertEqual(self.command.sanitize_name(u'John Doe'), u'John Doe')
-        self.assertEqual(self.command.sanitize_name(u' John Doe '), u'John Doe')
-        self.assertEqual(self.command.sanitize_name(u'John Doe 23'), u'John Doe')
+def test_parse_subject(self):
+    assert self.command.parse_subject('garbage') is None
 
-    def test_decode_subject(self):
-        text = u'メイル'
-        subject = Header(text, 'utf-8')
-        self.assertEqual(self.command.decode_subject(subject), text)
+    # Valid
+    valid = 'Receipt [$25.00] By: John Doe [john.doe@archlinux.org]'
+    output = command.parse_subject(valid)
+    assert output == 'John Doe'
 
-    def test_invalid_args(self):
-        stdin = sys.stdin
-        with self.assertRaises(CommandError) as e:
-            sys.stdin = StringIO('')
-            call_command('donor_import')
-        self.assertIn('Failed to read from STDIN', str(e.exception))
-        sys.stdin = stdin
 
-    def test_invalid_path(self):
-        with self.assertRaises(CommandError) as e:
-            call_command('donor_import', '/tmp/non-existant')
-        self.assertIn('argument input: can\'t open', str(e.exception))
+def test_parse_name():
+    assert command.sanitize_name('1244') == ''
+    assert command.sanitize_name('John Doe') == 'John Doe'
+    assert command.sanitize_name(' John Doe ') == 'John Doe'
+    assert command.sanitize_name('John Doe 23') == 'John Doe'
 
-    def test_import(self):
-        tmpmail = mkdtemp('archweb') + "/mail"
 
-        msg = Message()
-        msg['subject'] = 'John Doe'
-        msg['to'] = 'John Doe <john@doe.com>'
-        with open(tmpmail, 'wb') as fp:
-            fp.write(msg.as_bytes())
+def test_decode_subject():
+    text = u'メイル'
+    subject = Header(text, 'utf-8')
+    assert command.decode_subject(subject) == text
 
-        # Invalid
-        with self.assertRaises(SystemExit):
-            call_command('donor_import', tmpmail)
-        self.assertEqual(len(Donor.objects.all()), 0)
 
-        # Valid
-        msg = Message()
-        msg['subject'] = 'Receipt [$25.00] By: David Doe [david@doe.com]'
-        msg['to'] = 'John Doe <david@doe.com>'
-        with open(tmpmail, 'wb') as fp:
-            fp.write(msg.as_bytes())
+def test_invalid_args():
+    with pytest.raises(CommandError) as e:
+        call_command('donor_import')
+    assert 'Error: the following arguments are required' in str(e.value)
 
-        call_command('donor_import', tmpmail)
-        self.assertEqual(len(Donor.objects.all()), 1)
 
-        # Re-running should result in no new donor
-        call_command('donor_import', tmpmail)
-        self.assertEqual(len(Donor.objects.all()), 1)
+def test_invalid_path():
+    with pytest.raises(CommandError) as e:
+        call_command('donor_import', '/tmp/non-existant')
+    assert 'Failed to open maildir' in str(e.value)
+
+
+def test_maildir(db, tmp_path):
+    tmpdir = tmp_path / 'archweb'
+    tmpdir.mkdir()
+    mdir = tmpdir / 'maildir'
+
+    maildir = Maildir(mdir)
+    msg = Message()
+    msg['subject'] = 'John Doe'
+    msg['to'] = 'John Doe <john@doe.com>'
+    maildir.add(msg)
+
+    # Invalid
+    call_command('donor_import', mdir)
+    assert len(Donor.objects.all()) == 0
+
+    # Valid
+    msg = Message()
+    msg['subject'] = 'Receipt [$25.00] By: David Doe [david@doe.com]'
+    msg['to'] = 'John Doe <david@doe.com>'
+    maildir.add(msg)
+    call_command('donor_import', mdir)
+    assert len(Donor.objects.all()) == 1
+
+    # Re-running should result in no new donor
+    call_command('donor_import', mdir)
+    assert len(Donor.objects.all()) == 1
