40a955670af4af1954c39e2ec29472bd0b8e36c8
Jelle van der Waa <jelle@archlinux.org>
2019-09-10 22:23:43 +0200

    devel: Add sorting on groups to the admin view

--- devel/admin.py
+++ devel/admin.py
@@ -11,8 +11,8 @@ class UserProfileInline(admin.StackedInline):
 
 class UserProfileAdmin(UserAdmin):
     inlines = [UserProfileInline]
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'last_login', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
 
 
 class StaffGroupAdmin(admin.ModelAdmin):
