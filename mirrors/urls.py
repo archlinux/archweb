205ebb8f40982fe0c7b5b37af32b813ccaa012d2
Jelle van der Waa <jelle@archlinux.org>
2021-07-25 12:34:49 +0200

    Revert "Add caching for JSON mirror urls"
    This reverts commit 88d9c7692a8b8f4a102408e048f9aa4146fcb6f2. This
    introduced a bug into no longer returning the correct data for the
    authenticated reply which adds more data.

--- mirrors/urls.py
+++ mirrors/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.views.decorators.cache import cache_page
 
 from .views import mirrors, status, mirror_details, url_details
 from .views.api import status_json, mirror_details_json, locations_json
@@ -11,9 +10,9 @@ urlpatterns = [
     url(r'^status/json/$', status_json, name='mirror-status-json'),
     url(r'^status/tier/(?P<tier>\d+)/$', status, name='mirror-status-tier'),
     url(r'^status/tier/(?P<tier>\d+)/json/$', status_json, name='mirror-status-tier-json'),
-    url(r'^locations/json/$', cache_page(317)(locations_json), name='mirror-locations-json'),
+    url(r'^locations/json/$', locations_json, name='mirror-locations-json'),
     url(r'^(?P<name>[\.\-\w]+)/$', mirror_details),
-    url(r'^(?P<name>[\.\-\w]+)/json/$', cache_page(317)(mirror_details_json)),
+    url(r'^(?P<name>[\.\-\w]+)/json/$', mirror_details_json),
     url(r'^(?P<name>[\.\-\w]+)/(?P<url_id>\d+)/$', url_details),
 ]
 
