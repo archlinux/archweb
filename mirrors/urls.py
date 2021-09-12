c1e0f3ac7fd92109ec303c151c6c735485da326c
Jelle van der Waa <jelle@archlinux.org>
2021-09-12 11:37:58 +0200

    Enable caching for /locations/json endpoint

--- mirrors/urls.py
+++ mirrors/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from .views import mirrors, status, mirror_details, url_details
 from .views.api import status_json, mirror_details_json, locations_json
@@ -10,7 +11,7 @@ urlpatterns = [
     url(r'^status/json/$', status_json, name='mirror-status-json'),
     url(r'^status/tier/(?P<tier>\d+)/$', status, name='mirror-status-tier'),
     url(r'^status/tier/(?P<tier>\d+)/json/$', status_json, name='mirror-status-tier-json'),
-    url(r'^locations/json/$', locations_json, name='mirror-locations-json'),
+    url(r'^locations/json/$', cache_page(317)(locations_json), name='mirror-locations-json'),
     url(r'^(?P<name>[\.\-\w]+)/$', mirror_details),
     url(r'^(?P<name>[\.\-\w]+)/json/$', mirror_details_json),
     url(r'^(?P<name>[\.\-\w]+)/(?P<url_id>\d+)/$', url_details),
