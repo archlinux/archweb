1c16824206de5399de8007f4d35dfb40abccfdaa
Jelle van der Waa <jelle@archlinux.org>
2021-09-19 23:06:35 +0200

    Port planet tests to pytest

--- planet/tests/test_command.py
+++ planet/tests/test_command.py
@@ -1,10 +1,35 @@
 import time
 from unittest import mock
 
-from django.test import TestCase
+import pytest
+import feedparser
 
-from planet.models import Feed, FeedItem
+from planet.models import Feed, FeedItem, FEEDITEM_SUMMARY_LIMIT
 from planet.management.commands.update_planet import Command
+
+
+@pytest.fixture
+def command():
+    yield Command()
+
+
+@pytest.fixture
+def feed(db):
+    return Feed(title='test', website='http://archlinux.org',
+                website_rss='http://archlinux.org/feed.rss')
+
+
+class MockParse:
+    @staticmethod
+    def parse():
+        return {}
+
+
+@pytest.fixture
+def mock_parse(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockParse()
+    monkeypatch.setattr(feedparser, "parse", mock_get)
 
 
 class Result(dict):
@@ -27,60 +52,84 @@ class Entry(dict):
         return getattr(self, value)
 
 
-class UpdatePlanetTest(TestCase):
-
-    def setUp(self):
-        self.command = Command()
-        self.feed = Feed(title='test', website='http://archlinux.org',
-                         website_rss='http://archlinux.org/feed.rss')
-
-    # Test when feedparser receives an exception and returns no status
-    @mock.patch('feedparser.parse')
-    def test_parse_feed_wrong(self, parse):
+# Test when feedparser receives an exception and returns no status
+def test_parse_feed_wrong(feed, command):
+    with mock.patch('feedparser.parse') as parse:
         parse.return_value = {}
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 0
 
-    @mock.patch('feedparser.parse')
-    def test_parse_feed_304(self, parse):
+
+def test_parse_feed_304(feed, command):
+    with mock.patch('feedparser.parse') as parse:
         parse.return_value = {'status': 304}
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 0
 
-    @mock.patch('feedparser.parse')
-    def test_parse_feed_unknown(self, parse):
+
+def test_parse_feed_unknown(feed, command):
+    with mock.patch('feedparser.parse') as parse:
         parse.return_value = {'status': 201}
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 0
 
-    @mock.patch('feedparser.parse')
-    def test_parse_entries_empty(self, parse):
+
+def test_parse_entries_empty(feed, command):
+    with mock.patch('feedparser.parse') as parse:
         parse.return_value = Result()
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 0
 
-    @mock.patch('feedparser.parse')
-    def test_parse_entries_not_published(self, parse):
+
+def test_parse_entries_not_published(feed, command):
+    with mock.patch('feedparser.parse') as parse:
+        feed.save()
         value = Result()
         entry = Entry()
         entry.published_parsed = None
         value.entries = [entry]
         parse.return_value = value
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 0
 
-    @mock.patch('feedparser.parse')
-    def test_parse_entries(self, parse):
-        self.feed.save()
+
+def test_parse_entries(feed, command):
+    with mock.patch('feedparser.parse') as parse:
+        feed.save()
         value = Result()
         value.entries = [Entry()]
         parse.return_value = value
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 1
 
-    @mock.patch('feedparser.parse')
-    def test_parse_entries_atom(self, parse):
-        self.feed.save()
+
+def test_parse_no_new_entries(feed, command):
+    with mock.patch('feedparser.parse') as parse:
+        feed.save()
+        value = Result()
+        value.entries = [Entry()]
+        parse.return_value = value
+        command.parse_feed(feed)
+        assert FeedItem.objects.count() == 1
+        command.parse_feed(feed)
+        assert FeedItem.objects.count() == 1
+
+
+def test_parse_no_link(feed, command):
+    with mock.patch('feedparser.parse') as parse:
+        feed.save()
+        value = Result()
+        entry = Entry()
+        entry.link = ''
+        value.entries = [entry]
+        parse.return_value = value
+        command.parse_feed(feed)
+        assert FeedItem.objects.count() == 0
+
+
+def test_parse_entries_atom(feed, command):
+    with mock.patch('feedparser.parse') as parse:
+        feed.save()
         value = Result()
         entry = Entry()
         entry.published_parsed = None
@@ -88,14 +137,29 @@ class UpdatePlanetTest(TestCase):
 
         value.entries = [entry]
         parse.return_value = value
-        self.command.parse_feed(self.feed)
+        command.parse_feed(feed)
         assert FeedItem.objects.count() == 1
 
-    @mock.patch('feedparser.parse')
-    def test_parse_feed_301(self, parse):
+
+def test_parse_feed_301(feed, command):
+    with mock.patch('feedparser.parse') as parse:
         return_value = Result()
         return_value.status = 301
         return_value.href = 'https://example.com/rss'
         parse.return_value = return_value
-        self.command.parse_feed(self.feed)
-        assert self.feed.website_rss == return_value.href
+        command.parse_feed(feed)
+        assert feed.website_rss == return_value.href
+
+
+def test_parse_item_summary(feed, command):
+    with mock.patch('feedparser.parse') as parse:
+        feed.save()
+        value = Result()
+        entry = Entry()
+        entry.description = ' '.join(['x' * 4 for x in range(0, FEEDITEM_SUMMARY_LIMIT * 4, 4)])
+
+        value.entries = [entry]
+        parse.return_value = value
+        command.parse_feed(feed)
+        feed = FeedItem.objects.first()
+        assert len(feed.summary) < FEEDITEM_SUMMARY_LIMIT
