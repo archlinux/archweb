4609fc77ef38520e8fa2875e7752affa678499bd
Abhishek Srikanth <abhisrik99@outlook.com>
2021-10-31 22:01:18 +0530

    Fixed linting errors

--- todolists/urls.py
+++ todolists/urls.py
@@ -8,17 +8,17 @@ urlpatterns = [
     re_path(r'^$', TodolistListView.as_view(), name='todolist-list'),
 
     re_path(r'^add/$',
-        permission_required('todolists.add_todolist')(add)),
+            permission_required('todolists.add_todolist')(add)),
     re_path(r'^(?P<slug>[-\w]+)/$', view),
     re_path(r'^(?P<slug>[-\w]+)/json$', view_json),
     re_path(r'^(?P<slug>[-\w]+)/edit/$',
-        permission_required('todolists.change_todolist')(edit)),
+            permission_required('todolists.change_todolist')(edit)),
     re_path(r'^(?P<slug>[-\w]+)/delete/$',
-        permission_required('todolists.delete_todolist')(DeleteTodolist.as_view())),
+            permission_required('todolists.delete_todolist')(DeleteTodolist.as_view())),
     re_path(r'^(?P<slug>[-\w]+)/flag/(?P<pkg_id>\d+)/$',
-        permission_required('todolists.change_todolistpackage')(flag)),
+            permission_required('todolists.change_todolistpackage')(flag)),
     re_path(r'^(?P<slug>[-\w]+)/pkgbases/(?P<svn_root>[a-z]+)/$',
-        list_pkgbases),
+            list_pkgbases),
 ]
 
 # vim: set ts=4 sw=4 et:
