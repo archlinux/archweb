f68e692f53ce68528d5faabe9b974c6d12cea601
Malte Jürgens <maltejur@dismail.de>
2021-11-22 21:33:55 +0100

    fix relative path

--- commonstyle/templatetags/archnavbar.py
+++ commonstyle/templatetags/archnavbar.py
@@ -1,14 +1,16 @@
+from os import path
 from django import template
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.simple_tag
 def archnavbar():
-    with open("./commonstyle/archlinux-common-style/html/navbar.html", "r", encoding="UTF-8") as f:
+    with open(path.join(settings.DEPLOY_PATH, "commonstyle", "archlinux-common-style", "html", "navbar.html"), "r", encoding="UTF-8") as f:
         return mark_safe(
-                f.read()
-                    .replace('href="https://archlinux.org"', 'href="/"')
-                    .replace('href="https://archlinux.org/packages/"', 'href="/packages/"')
-            )
+            f.read()
+            .replace('href="https://archlinux.org"', 'href="/"')
+            .replace('href="https://archlinux.org/packages/"', 'href="/packages/"')
+        )
