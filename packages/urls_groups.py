b2a784d75ba14ff6cd6d1e818f100df9c80d585d
Anton Hvornum <anton@hvornum.se>
2022-02-06 17:20:20 +0100

    Added a JSON search endpoint for groups. The result from the group search is all packages that belong to a specific group.

--- packages/urls_groups.py
+++ packages/urls_groups.py
@@ -1,8 +1,10 @@
 from django.urls import re_path, path
+from packages.views import search
 from packages.views.display import groups, group_details
 
 urlpatterns = [
     path('', groups, name='groups-list'),
+    path('search/json/', search.group_search_json),
     re_path(r'^(?P<arch>[A-z0-9]+)/$', groups),
     re_path(r'^(?P<arch>[A-z0-9]+)/(?P<name>[^ /]+)/$', group_details),
 ]
