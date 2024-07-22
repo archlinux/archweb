8b26cf9b9b915ba2e3b67466f19abae160e21207
Jelle van der Waa <jelle@archlinux.org>
2024-07-22 23:11:28 +0200

    releng: cache the torrent urls

--- releng/urls.py
+++ releng/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from releng import views
 
@@ -8,8 +9,8 @@ from .views import ReleaseDetailView, ReleaseListView
 releases_patterns = [
     path('', ReleaseListView.as_view(), name='releng-release-list'),
     path('json/', views.releases_json, name='releng-release-list-json'),
-    re_path(r'^(?P<version>[-.\w]+)/$', ReleaseDetailView.as_view(), name='releng-release-detail'),
-    re_path(r'^(?P<version>[-.\w]+)/torrent/$', views.release_torrent, name='releng-release-torrent'),
+    re_path(r'^(?P<version>[-.\w]+)/$', cache_page(311)(ReleaseDetailView.as_view()), name='releng-release-detail'),
+    re_path(r'^(?P<version>[-.\w]+)/torrent/$', cache_page(311)(views.release_torrent), name='releng-release-torrent'),
 ]
 
 netboot_patterns = [
