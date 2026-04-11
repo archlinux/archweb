6ec23f5b77aac2d9f51eff7641ffd04469714f7a
Ben Mezger <me@benmezger.nl>
2026-04-11 13:35:31 +0200

    Drop svn_root and use pkgbase for todos

--- main/models.py
+++ main/models.py
@@ -74,8 +74,6 @@ class Repo(models.Model):
         default=1, help_text="Flyspray project ID for this repository.")
     bugs_category = models.SmallIntegerField(
         default=2, help_text="Flyspray category ID for this repository.")
-    svn_root = models.CharField(
-        max_length=64, help_text="SVN root (e.g. path) for this repository.")
 
     def __str__(self):
         return self.name
