5757a137a6ada76eb13b42cb4ba510e466e48e9e
m1rm <mail@miriam-mueller.com>
2026-04-30 19:32:48 +0200

    bugfix(#573): add a test for newline preservation
    bugfix(#573): add a test for newline preservation

--- public/tests.py
+++ public/tests.py
@@ -1,3 +1,19 @@
+from django.template import Context, Template
+
+
+def test_news_preview_filter_chain_preserves_multiline_pre():
+    html = (
+        "<p>Intro</p>"
+        "<pre><code>line one\nline two\nline three</code></pre>"
+        "<p>Outro</p>"
+    )
+    template = Template("{{ html|linebreaks|truncatewords_html:100 }}")
+    rendered = template.render(Context({"html": html}))
+
+    assert "line one<br>line two<br>line three" in rendered
+    assert "line one line two line three" not in rendered
+
+
 def test_index(client, arches, repos, package, groups, staff_groups):
     response = client.get('/')
     assert response.status_code == 200
