6c275becf963f93772e84c70a90819495debc477
Jelle van der Waa <jelle@archlinux.org>
2026-05-23 20:33:34 +0200

    main: Type mastodon_link

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,5 +1,6 @@
 from django.conf import settings
+from django.http import HttpRequest
 
 
-def mastodon_link(request):
+def mastodon_link(request: HttpRequest) -> dict[str, str]:
     return {'MASTODON_LINK': getattr(settings, "MASTODON_LINK", "")}
