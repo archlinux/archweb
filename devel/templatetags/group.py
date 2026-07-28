e39c4b2c3dfb34e5ceecd45f679179c797b1912e
Jelle van der Waa <jelle@archlinux.org>
2026-07-28 21:47:08 +0200

    devel: Type templatetags

--- devel/templatetags/group.py
+++ devel/templatetags/group.py
@@ -1,14 +1,15 @@
 from django import template
+from django.contrib.auth.models import User
 
 register = template.Library()
 
 
 @register.filter(name='in_group')
-def in_group(user, group_name):
+def in_group(user: User, group_name: str) -> bool:
     return user.groups.filter(name=group_name).exists()
 
 
 @register.filter(name='in_groups')
-def in_groups(user, group_names):
-    group_names = group_names.split(':')
-    return user.groups.filter(name__in=group_names).exists()
+def in_groups(user: User, group_names: str) -> bool:
+    names = group_names.split(':')
+    return user.groups.filter(name__in=names).exists()
