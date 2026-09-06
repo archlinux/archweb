e2e36c6c7fb57715401d6012458430f680acf415
Jelle van der Waa <jelle@archlinux.org>
2026-09-06 17:39:42 +0200

    devel: Port test_templatetags to pytest

--- devel/tests/test_templatetags.py
+++ devel/tests/test_templatetags.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
 
 from devel.templatetags.group import in_group
 
 
-class DevelTemplatetagsTest(TestCase):
-    def test_in_group(self):
-        user = User.objects.create(username="joeuser", first_name="Joe",
-                                   last_name="User", email="user1@example.com")
-        self.assertEqual(in_group(user, 'none'), False)
+def test_in_group(db):
+    user = User.objects.create(username="joeuser", first_name="Joe",
+                               last_name="User", email="user1@example.com")
+    assert in_group(user, 'none') is False
+    user.delete()
